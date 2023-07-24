2ce21ddc1e0d1514bdc79999fe36da89c92e3af9
bswck <bswck.dev@gmail.com>
2023-07-24 08:32:42 +0200

    Improve nox release script

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,8 @@
 import argparse
+from typing import cast
 
 import nox
+from nox.command import CommandFailed
 
 
 @nox.session
@@ -18,25 +20,56 @@ def release(session: nox.Session) -> None:
         "version",
         type=str,
         nargs=1,
-        help="The type of semver release to make.",
-        choices={"major", "minor", "patch"},
     )
     args: argparse.Namespace = parser.parse_args(args=session.posargs)
     version: str = args.version.pop()
 
     # If we get here, we should be good to go
     # Let's do a final check for safety
-    confirm = input(f"You are about to bump the {version!r} version. Are you sure? [y/n]: ")
+    release_confirm = (
+        input(f"You are about to release {version!r} version. Are you sure? [y/n]: ")
+        .casefold()
+        .strip()
+    )
 
     # Abort on anything other than 'y'
-    if confirm.lower().strip() != "y":
+    if release_confirm != "y":
         session.error(f"You said no when prompted to bump the {version!r} version.")
 
     session.run("poetry", "self", "add", "poetry-bumpversion", external=True)
 
     session.log(f"Bumping the {version!r} version")
     session.run("poetry", "version", version, external=True)
-
-    session.log("Pushing the new tag")
-    session.run("git", "push", external=True)
+    new_version = (
+        "v"
+        + cast(
+            str, session.run("poetry", "version", "--short", silent=True, external=True)
+        ).strip()
+    )
+    session.log(f"Creating {new_version} tag...")
+    try:
+        session.run(
+            "git", "tag", "-a", new_version, "-m", f"Release {new_version}", external=True
+        )
+    except CommandFailed:
+        session.log(f"Failed to create {new_version} tag.")
+    session.log("Pushing local tags...")
     session.run("git", "push", "--tags", external=True)
+
+    session.run("git", "diff", external=True)
+    commit_confirm = (
+        input(
+            "You are about to commit auto-changed files, see the diff view above. "
+            "Are you sure? [y/n]: "
+        )
+        .casefold()
+        .strip()
+    )
+
+    if commit_confirm == "y":
+        session.run(
+            "git", "commit", "-a", "-m", f"Release {new_version}", external=True
+        )
+        session.run("git", "push", external=True)
+    else:
+        session.log("Ok.")
