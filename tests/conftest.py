7c825f4d8f6d93bb47d12a4f1d162190754f320e
bswck <bswck.dev@gmail.com>
2023-09-10 06:03:38 +0200

    Complete rewrite

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,39 +3,39 @@ from math import inf
 
 import pytest
 
-from commonvars import commonvar
+from proxyvars import lookup_proxy
 
 
 @pytest.fixture(params=[-100, 0, 5, 100])
 def integers(request):
     context_var: ContextVar[int] = ContextVar("integer")
     context_var.set(request.param)
-    common_var = commonvar(context_var)
-    yield request.param, context_var, common_var
+    proxy_var = lookup_proxy(context_var)
+    yield request.param, context_var, proxy_var
 
 
 @pytest.fixture(params=[-100.0, 0.0, 5.0, 100.0, inf])
 def floats(request):
     context_var: ContextVar[float] = ContextVar("float")
     context_var.set(request.param)
-    common_var = commonvar(context_var)
-    yield request.param, context_var, common_var
+    proxy_var = lookup_proxy(context_var)
+    yield request.param, context_var, proxy_var
 
 
 @pytest.fixture(params=["", "a", "abc"])
 def strings(request):
     context_var: ContextVar[str] = ContextVar("string")
     context_var.set(request.param)
-    common_var = commonvar(context_var)
-    yield request.param, context_var, common_var
+    proxy_var = lookup_proxy(context_var)
+    yield request.param, context_var, proxy_var
 
 
 @pytest.fixture(params=[True, False])
 def booleans(request):
     context_var: ContextVar[bool] = ContextVar("boolean")
     context_var.set(request.param)
-    common_var = commonvar(context_var)
-    yield request.param, context_var, common_var
+    proxy_var = lookup_proxy(context_var)
+    yield request.param, context_var, proxy_var
 
 
 @pytest.fixture(params=[[], [1], [1, 2], ["foo", "bar"]])
@@ -43,5 +43,5 @@ def lists(request):
     context_var: ContextVar[list] = ContextVar("list")
     param = request.param.copy()
     context_var.set(param)
-    common_var = commonvar(context_var)
-    yield param, context_var, common_var
+    proxy_var = lookup_proxy(context_var)
+    yield param, context_var, proxy_var
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -5,7 +5,7 @@ from random import choice
 import pytest
 from pytest_lazyfixture import lazy_fixture
 
-from commonvars import commonvar
+from proxyvars import lookup_proxy
 
 try:
     import numpy as np  # type: ignore
@@ -31,44 +31,44 @@ strings_and_lists = pytest.mark.parametrize(
 
 @integers_and_floats
 def test_abs(objects):
-    actual, _, common_var = objects
-    assert abs(actual) == abs(common_var)
+    actual, _, proxy_var = objects
+    assert abs(actual) == abs(proxy_var)
 
 
 @integers_and_floats
 def test_add(objects):
-    actual, _, common_var = objects
-    assert actual + 1 == common_var + 1
+    actual, _, proxy_var = objects
+    assert actual + 1 == proxy_var + 1
 
 
 def test_and_(integers):
-    actual, _, common_var = integers
-    assert actual & 1 == common_var & 1
+    actual, _, proxy_var = integers
+    assert actual & 1 == proxy_var & 1
 
 
 def test_concat(strings):
-    actual, _, common_var = strings
-    assert actual + "a" == common_var + "a"
+    actual, _, proxy_var = strings
+    assert actual + "a" == proxy_var + "a"
 
 
 def test_contains(strings):
-    actual, _, common_var = strings
-    assert ("a" in actual) == ("a" in common_var)
+    actual, _, proxy_var = strings
+    assert ("a" in actual) == ("a" in proxy_var)
 
 
 def test_countOf(strings):
-    actual, _, common_var = strings
-    assert actual.count("a") == common_var.count("a")
+    actual, _, proxy_var = strings
+    assert actual.count("a") == proxy_var.count("a")
 
 
 def test_item_access(lists):
     # covers getitem, setitem and delitem
-    actual, _, common_var = lists
+    actual, _, proxy_var = lists
     actual[:1] = [1]
-    assert actual == common_var
-    assert actual[0] is common_var[0]
+    assert actual == proxy_var
+    assert actual[0] is proxy_var[0]
     del actual[0]
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 @pytest.mark.parametrize(
@@ -82,234 +82,234 @@ def test_item_access(lists):
     ],
 )
 def test_eq(objects):
-    actual, _, common_var = objects
-    assert actual == common_var
+    actual, _, proxy_var = objects
+    assert actual == proxy_var
 
 
 @integers_and_floats
 def test_floordiv(objects):
-    actual, _, common_var = objects
-    assert isinf(actual) == isinf(common_var)
+    actual, _, proxy_var = objects
+    assert isinf(actual) == isinf(proxy_var)
     if not isinf(actual):
-        assert actual // 1 == common_var // 1
+        assert actual // 1 == proxy_var // 1
 
 
 @integers_and_floats
 def test_ge(objects):
-    actual, _, common_var = objects
-    assert (actual >= 1) == (common_var >= 1)
+    actual, _, proxy_var = objects
+    assert (actual >= 1) == (proxy_var >= 1)
 
 
 def test_gt(integers):
-    actual, _, common_var = integers
-    assert (actual > 1) == (common_var > 1)
-    assert not actual > common_var  # they are equal
+    actual, _, proxy_var = integers
+    assert (actual > 1) == (proxy_var > 1)
+    assert not actual > proxy_var  # they are equal
 
 
 def test_iadd(integers):
-    actual, mgr, common_var = integers
+    actual, mgr, proxy_var = integers
 
     actual += 1
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var -= 1
+    proxy_var -= 1
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 def test_iand(integers):
-    actual, mgr, common_var = integers
+    actual, mgr, proxy_var = integers
 
     actual &= 1
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var &= 0
+    proxy_var &= 0
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 def test_iconcat(strings):
-    actual, mgr, common_var = strings
+    actual, mgr, proxy_var = strings
 
     actual += "a"
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var += "a"
+    proxy_var += "a"
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 def test_ifloordiv(integers):
-    actual, mgr, common_var = integers
+    actual, mgr, proxy_var = integers
 
     actual //= 1
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var //= 2
+    proxy_var //= 2
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 def test_ilshift(integers):
-    actual, mgr, common_var = integers
+    actual, mgr, proxy_var = integers
 
     actual <<= 1
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var <<= 1
+    proxy_var <<= 1
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 def test_imod(integers):
-    actual, mgr, common_var = integers
+    actual, mgr, proxy_var = integers
 
     actual %= 1
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var %= 1
+    proxy_var %= 1
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 def test_imul(integers):
-    actual, mgr, common_var = integers
+    actual, mgr, proxy_var = integers
 
     actual *= 100
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var *= 0.6
+    proxy_var *= 0.6
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 @strings_and_lists
 def test_index(objects):
-    actual, _, common_var = objects
+    actual, _, proxy_var = objects
     if actual:
         element = choice(actual)
-        assert actual.index(element) == common_var.index(element)
+        assert actual.index(element) == proxy_var.index(element)
 
 
 def test_inv(integers):
-    actual, _, common_var = integers
-    assert ~actual == ~common_var
+    actual, _, proxy_var = integers
+    assert ~actual == ~proxy_var
 
 
 def test_ior(integers):
-    actual, mgr, common_var = integers
+    actual, mgr, proxy_var = integers
 
     actual |= 1
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var |= 128
+    proxy_var |= 128
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 def test_ipow(integers):
-    actual, mgr, common_var = integers
+    actual, mgr, proxy_var = integers
 
     actual **= 1
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var **= 1
+    proxy_var **= 1
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 def test_irshift(integers):
-    actual, mgr, common_var = integers
+    actual, mgr, proxy_var = integers
 
     actual >>= 1
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var >>= 1
+    proxy_var >>= 1
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 def test_is_(integers):
-    actual, _, common_var = integers
+    actual, _, proxy_var = integers
     with pytest.raises(AssertionError):
         # this should fail because the objects are not the same
-        assert actual is common_var
+        assert actual is proxy_var
 
 
 def test_is_not(integers):
-    actual, _, common_var = integers
-    assert actual is not common_var
+    actual, _, proxy_var = integers
+    assert actual is not proxy_var
 
 
 def test_isub(integers):
-    actual, mgr, common_var = integers
+    actual, mgr, proxy_var = integers
 
     actual -= 1
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var += 1
+    proxy_var += 1
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 def test_itruediv(integers):
-    actual, mgr, common_var = integers
+    actual, mgr, proxy_var = integers
 
     actual /= 1
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var /= 2
+    proxy_var /= 2
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 def test_ixor(integers):
-    actual, mgr, common_var = integers
+    actual, mgr, proxy_var = integers
 
     actual ^= 1
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
-    common_var ^= 1
+    proxy_var ^= 1
     actual = mgr.get()
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 @integers_and_floats
 def test_le(objects):
-    actual, _, common_var = objects
-    assert (actual <= 1) == (common_var <= 1)
+    actual, _, proxy_var = objects
+    assert (actual <= 1) == (proxy_var <= 1)
 
 
 @strings_and_lists
 def test_length_hint(objects):
-    actual, _, common_var = objects
-    assert len(actual) == len(common_var)
+    actual, _, proxy_var = objects
+    assert len(actual) == len(proxy_var)
 
 
 def test_lshift(integers):
-    actual, _, common_var = integers
-    assert actual << 1 == common_var << 1
+    actual, _, proxy_var = integers
+    assert actual << 1 == proxy_var << 1
 
 
 @integers_and_floats
 def test_lt(objects):
-    actual, _, common_var = objects
-    assert (actual < 1) == (common_var < 1)
-    assert not actual < common_var  # they are equal
-    assert not common_var < actual  # they are equal
+    actual, _, proxy_var = objects
+    assert (actual < 1) == (proxy_var < 1)
+    assert not actual < proxy_var  # they are equal
+    assert not proxy_var < actual  # they are equal
 
 
 if np:
@@ -318,98 +318,98 @@ if np:
         actual = np.array([[1, 2], [3, 4]])
         mgr = ContextVar("matrix")
         mgr.set(actual)
-        common_var = commonvar(mgr)
+        proxy_var = lookup_proxy(mgr)
         matrix = [[9, 10], [11, 12]]
-        assert ((actual @ matrix) == (common_var @ matrix)).all()
+        assert ((actual @ matrix) == (proxy_var @ matrix)).all()
 
     def test_imatmul():
         actual = np.array([[1, 2], [3, 4]])
         mgr = ContextVar("matrix")
         mgr.set(actual)
-        common_var = commonvar(mgr)
+        proxy_var = lookup_proxy(mgr)
 
         actual @= [[5, 6], [7, 8]]  # type: ignore
         mgr.set(actual)
-        assert (actual == common_var).all()
+        assert (actual == proxy_var).all()
 
-        common_var @= [[9, 10], [11, 12]]
+        proxy_var @= [[9, 10], [11, 12]]
         actual = mgr.get()
-        assert (actual == common_var).all()
+        assert (actual == proxy_var).all()
 
 
 def test_methods(strings, integers):
-    actual, _, common_var = strings
-    assert actual.upper() == common_var.upper()
+    actual, _, proxy_var = strings
+    assert actual.upper() == proxy_var.upper()
 
-    actual, _, common_var = integers
-    assert actual.bit_length() == common_var.bit_length()
+    actual, _, proxy_var = integers
+    assert actual.bit_length() == proxy_var.bit_length()
 
 
 def test_mod(integers):
-    actual, _, common_var = integers
-    assert actual % 1 == common_var % 1
+    actual, _, proxy_var = integers
+    assert actual % 1 == proxy_var % 1
 
 
 def test_mul(strings):
-    actual, _, common_var = strings
-    assert actual * 2 == common_var * 2
+    actual, _, proxy_var = strings
+    assert actual * 2 == proxy_var * 2
 
 
 def test_ne(integers):
-    actual, _, common_var = integers
-    assert (actual != 1) == (common_var != 1)
+    actual, _, proxy_var = integers
+    assert (actual != 1) == (proxy_var != 1)
 
 
 def test_neg(integers):
-    actual, _, common_var = integers
-    assert -actual == -common_var
+    actual, _, proxy_var = integers
+    assert -actual == -proxy_var
 
 
 def test_not_(integers):
-    actual, _, common_var = integers
-    assert (not not actual) == (not not common_var)
+    actual, _, proxy_var = integers
+    assert (not not actual) == (not not proxy_var)
 
 
 def test_or(integers):
-    actual, _, common_var = integers
-    assert actual | 1 == common_var | 1
+    actual, _, proxy_var = integers
+    assert actual | 1 == proxy_var | 1
 
 
 def test_pos(integers):
-    actual, _, common_var = integers
-    assert +actual == +common_var
+    actual, _, proxy_var = integers
+    assert +actual == +proxy_var
 
 
 def test_pow(integers):
-    actual, _, common_var = integers
-    assert actual**1 == common_var**1
+    actual, _, proxy_var = integers
+    assert actual**1 == proxy_var**1
 
 
 def test_rshift(integers):
-    actual, _, common_var = integers
-    assert actual >> 1 == common_var >> 1
+    actual, _, proxy_var = integers
+    assert actual >> 1 == proxy_var >> 1
 
 
 def test_sub(integers):
-    actual, _, common_var = integers
-    assert actual - 1 == common_var - 1
+    actual, _, proxy_var = integers
+    assert actual - 1 == proxy_var - 1
 
 
 def test_truediv(integers):
-    actual, _, common_var = integers
-    assert actual / 1 == common_var / 1
+    actual, _, proxy_var = integers
+    assert actual / 1 == proxy_var / 1
 
 
 def test_truth(integers):
     # actual truth check is technically faster than bool()
     # but semantically equivalent
-    actual, _, common_var = integers
-    assert bool(actual) == bool(common_var)
+    actual, _, proxy_var = integers
+    assert bool(actual) == bool(proxy_var)
 
 
 def test_xor(integers):
-    actual, _, common_var = integers
-    assert actual ^ 1 == common_var ^ 1
+    actual, _, proxy_var = integers
+    assert actual ^ 1 == proxy_var ^ 1
 
 
 def test_attrgetter():
@@ -420,26 +420,26 @@ def test_attrgetter():
     actual = A(1)
     mgr = ContextVar("attrgetter")
     mgr.set(actual)
-    common_var = commonvar(mgr)
+    proxy_var = lookup_proxy(mgr)
 
-    assert actual.value == common_var.value
+    assert actual.value == proxy_var.value
 
 
 def test_delitem():
     actual = [1, 2, 3]
     mgr = ContextVar("delitem")
     mgr.set(actual)
-    common_var = commonvar(mgr)
+    proxy_var = lookup_proxy(mgr)
 
     del actual[0]
     mgr.set(actual)
-    assert actual == common_var
+    assert actual == proxy_var
 
 
 def test_eq_object():
     actual = object()
     mgr = ContextVar("eq")
     mgr.set(actual)
-    common_var = commonvar(mgr)
+    proxy_var = lookup_proxy(mgr)
 
-    assert actual == common_var
+    assert actual == proxy_var
