afcc015750cbdb13f0a68008f74fb06e7ef06853
bswck <bswck.dev@gmail.com>
2024-05-09 09:02:16 +0200

    Upgrade to https://github.com/bswck/skeleton/tree/0.0.2rc-239-ga084376
    Skeleton revision: https://github.com/bswck/skeleton/tree/0.0.2rc-239-ga084376

--- scripts/skeleton.0.0.2rc-239-ga084376.bash
+++ scripts/skeleton.0.0.2rc-239-ga084376.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # (C) 2023–present Bartosz Sławecki (bswck)
 #
-# Interact with bswck/skeleton (current version: https://github.com/bswck/skeleton/tree/0.0.2rc-238-g68b0ab8).
+# Interact with bswck/skeleton (current version: https://github.com/bswck/skeleton/tree/0.0.2rc-239-ga084376).
 #
-# This file was generated from bswck/skeleton@0.0.2rc-238-g68b0ab8.
+# This file was generated from bswck/skeleton@0.0.2rc-239-ga084376.
 # Instead of changing this particular file, you might want to alter the template:
-# https://github.com/bswck/skeleton/tree/0.0.2rc-238-g68b0ab8/project/scripts/skeleton.%7B%7Bsref%7D%7D.bash.jinja
+# https://github.com/bswck/skeleton/tree/0.0.2rc-239-ga084376/project/scripts/skeleton.%7B%7Bsref%7D%7D.bash.jinja
 #
 # Usage:
 #
@@ -141,32 +141,32 @@ make_context() {
 	)
 	export SKELETON_REF
 	SKELETON_REF=$(cat <<- 'EOF'
-		0.0.2rc-238-g68b0ab8
+		0.0.2rc-239-ga084376
 	EOF
 	)
 	export SREF
 	SREF=$(cat <<- 'EOF'
-		0.0.2rc-238-g68b0ab8
+		0.0.2rc-239-ga084376
 	EOF
 	)
 	export SKELETON_REV
 	SKELETON_REV=$(cat <<- 'EOF'
-		https://github.com/bswck/skeleton/tree/0.0.2rc-238-g68b0ab8
+		https://github.com/bswck/skeleton/tree/0.0.2rc-239-ga084376
 	EOF
 	)
 	export SREV
 	SREV=$(cat <<- 'EOF'
-		https://github.com/bswck/skeleton/tree/0.0.2rc-238-g68b0ab8
+		https://github.com/bswck/skeleton/tree/0.0.2rc-239-ga084376
 	EOF
 	)
 	export SKELETON_AND_REF
 	SKELETON_AND_REF=$(cat <<- 'EOF'
-		bswck/skeleton@0.0.2rc-238-g68b0ab8
+		bswck/skeleton@0.0.2rc-239-ga084376
 	EOF
 	)
 	export SNREF
 	SNREF=$(cat <<- 'EOF'
-		bswck/skeleton@0.0.2rc-238-g68b0ab8
+		bswck/skeleton@0.0.2rc-239-ga084376
 	EOF
 	)
 	export GH_REPO_ARGS
@@ -179,7 +179,7 @@ make_context() {
 		jq -n '{"deployment_branch_policy": {"protected_branches": false,"custom_branch_policies": true}}' | gh api -H "Accept: application/vnd.github+json" -X PUT "/repos/bswck/proxyvars/environments/$1" --silent --input -
 	EOF
 	)
-    export LAST_REF="0.0.2rc-238-g68b0ab8"
+    export LAST_REF="0.0.2rc-239-ga084376"
     export PROJECT_PATH_KEY="$$_skeleton_project_path"
     export NEW_REF_KEY="$$_skeleton_new_ref"
     export LAST_LICENSE_NAME="MIT"
